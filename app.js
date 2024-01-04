41249a37ca9b2579246775ed6c1b38aaa975068d
Bruno Oliveira <brunohfoliveira@gmail.com>
2024-01-04 17:49:30 -0300

    Revert "numero de escolha"
    This reverts commit 86a5cad81574772b9d686f3b74ed542a91b91ce6.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
 let listaDeNumerosSorteados = [];
 let numeroLimite = 10 ;
+
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -16,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
 exibirMensagemInicial();
